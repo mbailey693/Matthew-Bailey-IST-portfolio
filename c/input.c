a255f9679526f1e54e6d565db736d32cc39066ae
mbailey693 <2026bailey.matthew@fcboe.org>
2022-11-07 14:45:02 -0500

    Added input programs

---
+++ c/input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+	printf("enter an interger: ");
+	scanf("%i", &a);
+	printf("You typed %i\n", a);
+	return 0;
+}
--- c/squared.c
+++ c/squared.c
@@ -9,8 +9,8 @@ int main()
 {
 	int a;
 	int b;
-	a = 6;
-	b = squared(2);
+	a = 5;
+	b = squared(8);
 	int c = a + b;
 	printf("%i\n", c);
 	return 0;
